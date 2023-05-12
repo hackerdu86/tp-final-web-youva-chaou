238f02d61d39606ea72debb60eca54b2146d415b
Youva Chaou <2125762@cmontmorency.qc.ca>
2023-05-12 13:51:57 -0400

    Implémentations des routes GET pour les étudiants et pour les stages

--- server/controllers/internships-controller.js
+++ server/controllers/internships-controller.js
@@ -1,12 +1,44 @@
 const HttpError = require("../models/http-error");
 const Student = require("../models/student");
 const Internship = require("../models/internship");
-const { get } = require("mongoose");
+const { internshipExists } = require("../utils/existances-controller");
+const studentExists = require("../utils/existances-controller").internshipExists;
 let ObjectId = require("mongoose").Types.ObjectId;
 
 //GET functions
-async function getInternships(req, res, next) {}
-async function getInternship(req, res, next) {}
+async function getInternships(req, res, next) {
+    let internships = [];
+  try {
+    internships = await Internship.find({});
+    res.json({
+        internships: internships.map((internship) => {
+        return internship.toObject({ getters: true });
+      }),
+    });
+  } catch (err) {
+    console.log(err);
+    new HttpError("Erreur lors de la récupération des stages", 500);
+  }
+}
+async function getInternship(req, res, next) {
+    const internshipId = req.params.id;
+    try {
+      const exists = await internshipExists(internshipId);
+      if (!exists) {
+        next(new HttpError("Ce stage n'existe pas", 404));
+      }
+      const internship = await Internship.findById(internshipId);
+      res.json({ internship: internship.toObject({ getters: true }) });
+    } catch (err) {
+      console.log(err);
+      next(
+        new HttpError(
+          "Erreur lors de la vérification de l'existance du stage",
+          500
+        )
+      );
+    }
+}
 
 //POST functions
 async function addInternship(req, res, next) {}
--- server/controllers/students-controller.js
+++ server/controllers/students-controller.js
@@ -1,12 +1,44 @@
 const HttpError = require("../models/http-error");
 const Student = require("../models/student");
 const Internship = require("../models/internship");
-let ObjectId = require("mongoose").Types.ObjectId;
+const ObjectId = require("mongoose").Types.ObjectId;
+const studentExists = require("../utils/existances-controller").studentExists;
 
 //GET functions
-async function getStudents(req, res, next) {}
+async function getStudents(req, res, next) {
+  let students = [];
+  try {
+    students = await Student.find({});
+    res.json({
+      students: students.map((student) => {
+        return student.toObject({ getters: true });
+      }),
+    });
+  } catch (err) {
+    console.log(err);
+    new HttpError("Erreur lors de la récupération des étudiants", 500);
+  }
+}
 
-async function getStudent(req, res, next) {}
+async function getStudent(req, res, next) {
+  const studentId = req.params.id;
+  try {
+    const exists = await studentExists(studentId);
+    if (!exists) {
+      next(new HttpError("Cet étudiant n'existe pas", 404));
+    }
+    const student = await Student.findById(studentId);
+    res.json({ student: student.toObject({ getters: true }) });
+  } catch (err) {
+    console.log(err);
+    next(
+      new HttpError(
+        "Erreur lors de la vérification de l'existance de l'étudiant",
+        500
+      )
+    );
+  }
+}
 
 //POST functions
 async function addStudent(req, res, next) {}
