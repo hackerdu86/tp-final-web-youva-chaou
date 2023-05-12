130165eb105571c4d1c881bd459fa104811d1679
Youva Chaou <2125762@cmontmorency.qc.ca>
2023-05-12 11:13:54 -0400

    Implémentatinos des champs des modèles Student et Internship

--- server/models/internship.js
+++ server/models/internship.js
@@ -2,6 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const internshipSchema = new Schema({});
+const internshipSchema = new Schema({
+    contactFullName: { type: String, required: true },
+    contactEmail: { type: String, required: true },
+    companyName: { type: String, required: true },
+    companyAdress: { type: String, required: true },
+    internshipType: { type: String, required: true },
+    availablePositions: { type: Number, required: true },
+    internshipDescription: { type: String, required: true },
+    internshipHourWage: { type: Number, required: true },
+    registeredStudents: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Student",
+      required: false,
+    },
+  ],
+});
 
 module.exports = mongoose.model("Internship", internshipSchema);
--- server/models/student.js
+++ server/models/student.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const studentSchema = new Schema({});
+const studentSchema = new Schema({
+  daNumber: { type: String, required: true },
+  studentFullName: { type: String, required: true },
+  studentEmail: { type: String, required: true },
+  exitProfil: { type: String, required: true },
+  registeredInterships: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Internship",
+      required: false,
+    },
+  ],
+});
 
 module.exports = mongoose.model("Student", studentSchema);
