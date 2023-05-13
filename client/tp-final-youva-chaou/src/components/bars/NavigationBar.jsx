0c9cb844f567994f66f0486bd84f656d78ec2b83
Youva Chaou <2125762@cmontmorency.qc.ca>
2023-05-13 12:55:31 -0400

    Création de la barre de navigation

--- client/tp-final-youva-chaou/src/components/bars/NavigationBar.jsx
+++ client/tp-final-youva-chaou/src/components/bars/NavigationBar.jsx
@@ -1,7 +1,130 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function NavigationBar() {
   return (
-<></>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Collège Montmorency
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Page informatives
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Déroulement des stages {"(Aux employeurs)"}
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Déroulement des stages {"(Aux étudiants)"}
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Profils et compétence des stagiaires
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    FAQ
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Étudiant
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Inscrire un étudiant
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Étudiants inscrits
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Stages
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Ajouter un stage
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Stages disponibles
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
--- client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddStudentForm.jsx
+++ client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddStudentForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function AddStudentForm() {
   function onRegisterButtonClick() {
     console.log("dsds");
