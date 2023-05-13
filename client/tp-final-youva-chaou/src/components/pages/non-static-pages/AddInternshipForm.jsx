3df3a1dab2b18b566c2b82bfe215b014f6cb41eb
Youva Chaou <2125762@cmontmorency.qc.ca>
2023-05-13 15:40:12 -0400

    Création de la fiche d'inscription pour un nouveau stage

--- client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddInternshipForm.jsx
+++ client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddInternshipForm.jsx
@@ -1,9 +1,159 @@
 function AddInternshipForm() {
-    return (
-        <>
-        AddInternshipForm
-        </>
-    )
+  return (
+    <form>
+      <div className="card">
+        <div className="card-header">Ajout d'un nouveau stage</div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlhtmlFor="text1">Nom de l'entreprise</label>
+              <input
+                id="text1"
+                name="text1"
+                type="text"
+                required="required"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="text5">Adresse de l’entreprise</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <i className="fa fa-map-marker"></i>
+                  </div>
+                </div>
+                <input
+                  id="text5"
+                  name="text5"
+                  type="text"
+                  required="required"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="text">Nom de la personne contact</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <i className="fa fa-address-card"></i>
+                  </div>
+                </div>
+                <input
+                  id="text"
+                  name="text"
+                  placeholder="Prénom et nom de famille"
+                  type="text"
+                  required="required"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="text2">Courriel de la personne contact</label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <i className="fa fa-at"></i>
+                  </div>
+                </div>
+                <input
+                  id="text2"
+                  name="text2"
+                  placeholder="exemple@domain.com"
+                  type="text"
+                  required="required"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="text4">
+                Numéro de téléphone de la personne contact
+              </label>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <i className="fa fa-phone"></i>
+                  </div>
+                </div>
+                <input
+                  id="text4"
+                  name="text4"
+                  placeholder="xxx-xxx-xxxx"
+                  type="text"
+                  required="required"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Type de stage</label>
+              <div>
+                <div className="custom-control custom-radio custom-control-inline">
+                  <input
+                    name="radio"
+                    id="radio_0"
+                    type="radio"
+                    className="custom-control-input"
+                    value="profil1"
+                    defaultChecked
+                  />
+                  <label htmlFor="radio_0" className="custom-control-label">
+                    Développement d'applications
+                  </label>
+                </div>
+                <div className="custom-control custom-radio custom-control-inline">
+                  <input
+                    name="radio"
+                    id="radio_1"
+                    type="radio"
+                    className="custom-control-input"
+                    value="profil2"
+                  />
+                  <label htmlFor="radio_1" className="custom-control-label">
+                    Réseaux et sécurité
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="textarea">Description du stage</label>
+              <textarea
+                id="textarea"
+                name="textarea"
+                cols="40"
+                rows="5"
+                className="form-control"
+                aria-describedby="textareaHelpBlock"
+                required="required"
+              ></textarea>
+              <span id="textareaHelpBlock" className="form-text text-muted">
+                Fournissez une brève description du stage, incluez si vous le
+                souhaitez les différentes tâches associés à celui-ci et les
+                exigences générales.
+              </span>
+            </div>
+            <div className="form-group">
+              <label htmlFor="text3">Salaire</label>
+              <input
+                id="text3"
+                name="text3"
+                placeholder="$/heure (approximatif)"
+                type="text"
+                className="form-control"
+                required="required"
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary">Ajouter le stage</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default AddInternshipForm;
--- client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddStudentForm.jsx
+++ client/tp-final-youva-chaou/src/components/pages/non-static-pages/AddStudentForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 function AddStudentForm() {
   function onRegisterButtonClick() {
     console.log("dsds");
@@ -11,100 +11,83 @@ function AddStudentForm() {
       <div className="card">
         <div className="card-header">Inscription d'un étudiant</div>
         <div className="card-body">
-          <div className="form-group row">
-            <label htmlFor="text" className="col-4 col-form-label">
-              Da de l'étudiant
-            </label>
-            <div className="col-8">
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text">
-                    <i className="fa fa-id-card-o"></i>
-                  </div>
+          <div class="form-group">
+            <label for="text">Da de l'étudiant</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">
+                  <i class="fa fa-id-card-o"></i>
                 </div>
-                <input
-                  id="text"
-                  name="text"
-                  type="text"
-                  className="form-control"
-                  required="required"
-                  aria-describedby="textHelpBlock"
-                ></input>
               </div>
-              <span id="textHelpBlock" className="form-text text-muted">
-                Chiffres uniquement
-              </span>
-            </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="text1" className="col-4 col-form-label">
-              Nom complet de l'étudiant
-            </label>
-            <div className="col-8">
               <input
-                id="text1"
-                name="text1"
-                placeholder="Nom de famille et prénom"
+                id="text"
+                name="text"
                 type="text"
-                className="form-control"
+                aria-describedby="textHelpBlock"
                 required="required"
-              ></input>
+                class="form-control"
+              />
             </div>
+            <span id="textHelpBlock" class="form-text text-muted">
+              Chiffres uniquement
+            </span>
           </div>
-          <div className="form-group row">
-            <label htmlFor="text2" className="col-4 col-form-label">
-              Courriel de l'étudiant
-            </label>
-            <div className="col-8">
-              <input
-                id="text2"
-                name="text2"
-                placeholder="exemple@cmontmorency.qc.ca"
-                type="text"
-                className="form-control"
-                required="required"
-              ></input>
-            </div>
+          <div class="form-group">
+            <label for="text1">Nom complet de l'étudiant</label>
+            <input
+              id="text1"
+              name="text1"
+              placeholder="Prénom et nom de famille"
+              type="text"
+              required="required"
+              class="form-control"
+            />
           </div>
-          <div className="form-group row">
-            <label className="col-4">Profil de l'étudiant</label>
-            <div className="col-8">
-              <div className="custom-control custom-radio custom-control-inline">
+          <div class="form-group">
+            <label for="text2">Courriel de l'étudiant</label>
+            <input
+              id="text2"
+              name="text2"
+              placeholder="exemple@cmontmorency.qc.ca"
+              type="text"
+              required="required"
+              class="form-control"
+            />
+          </div>
+          <div class="form-group">
+            <label>Profil de l'étudiant</label>
+            <div>
+              <div class="custom-control custom-radio custom-control-inline">
                 <input
                   name="radio"
                   id="radio_0"
                   type="radio"
-                  className="custom-control-input"
+                  class="custom-control-input"
                   value="profil1"
                   defaultChecked
-                ></input>
-                <label htmlFor="radio_0" className="custom-control-label">
+                />
+                <label for="radio_0" class="custom-control-label">
                   Développement d'applications
                 </label>
               </div>
-              <div className="custom-control custom-radio custom-control-inline">
+              <div class="custom-control custom-radio custom-control-inline">
                 <input
                   name="radio"
                   id="radio_1"
                   type="radio"
-                  className="custom-control-input"
+                  class="custom-control-input"
                   value="profil2"
-                ></input>
-                <label htmlFor="radio_1" className="custom-control-label">
+                />
+                <label for="radio_1" class="custom-control-label">
                   Réseaux et sécurité
                 </label>
               </div>
             </div>
           </div>
-          <div className="form-group row">
-            <div className="offset-4 col-8">
-              <button
-                onClick={onRegisterButtonClick}
-                className="btn btn-primary"
-              >
-                Inscrire l'étudiant
-              </button>
-            </div>
+          <div class="form-group">
+            <button onClick={onRegisterButtonClick} class="btn btn-primary">
+              Inscrire l'étudiant
+            </button>
           </div>
         </div>
       </div>
